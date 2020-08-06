49a5b17e46cef588bc61fac8b5c8297f32c38bd1
Shubhang Sharma <shubhang.sharma123@live.com>
2020-08-06 21:48:44 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './components/Header/Header';
+import Navigationbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navigationbar />
     </div>
   )
 }
