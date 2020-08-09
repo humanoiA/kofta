162145302f5319adb5c819c3a55cd93ffc966605
Shubhang Sharma <shubhang.sharma123@live.com>
2020-08-09 23:45:11 +0530

    Added fonts and created banner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import Particles from 'react-particles-js';
 import Navigationbar from './components/Navbar/Navbar';
+import Banner from './components/Banner/Banner';
 import Registration from './components/Registration/Registration';
-
+import ParticlesBg from 'particles-bg'
 function App() {
   return(<div className="App">
     <Navigationbar />
-    <Registration />
+          <ParticlesBg type="square" num={20} bg={true} />
+  <Banner></Banner>
   </div>)
 }
 
